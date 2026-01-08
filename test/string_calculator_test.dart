83776b0e407488a5b7c6de2d1276936c434a7e0e
Tanya Omar <tanyaomar17@gmail.com>
2026-01-08 13:41:14 +0530

    RED: test empty string returns 0

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -3,7 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:string_calculator_tdd/string_calculator.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = StringCalculator();
+  final calculator = StringCalculator();
+
+  test('should return 0 for empty string', () {
+    expect(calculator.add(''), 0);
   });
 }
