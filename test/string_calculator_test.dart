b003e7cde40ac19b33d0d452bb046632a49cfad6
Tanya Omar <tanyaomar17@gmail.com>
2026-01-08 13:29:32 +0530

    chore: intialize project structure

---
+++ test/string_calculator_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:string_calculator_tdd/string_calculator.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = StringCalculator();
+  });
+}
