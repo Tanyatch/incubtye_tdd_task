ee3360b2170e37889103813e0bc7d095ca1d76d2
Tanya Omar <tanyaomar17@gmail.com>
2026-01-08 13:56:35 +0530

    RED: test custom single-char delimiter

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -20,4 +20,8 @@ void main() {
   test('should handle new lines between numbers', () {
     expect(calculator.add('1\n2,3'), 6);
   });
+    test('supports custom single-char delimiter', () {
+    var calc = StringCalculator();
+    expect(calc.add('//;\n1;2'), 3);
+  });
 }
